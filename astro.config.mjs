cf8b321f21c63fc39e8ab9927c8170794638115f
golden05 <golden05@gmail.com>
2023-04-02 11:09:09 +0800

    feat: tailwind google fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react(), image()]
+  integrations: [mdx(), sitemap(), react(), image(), tailwind()]
 });
